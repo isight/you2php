42cb63b476fba39d87cfa8f826cb2d7f46d42989
David <zuobq@msn.com>
2019-07-05 10:57:04 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', "AIzaSyBR-iZQ2J_4PNFlBkKr4Yx6h4V9gwAGgKc");
+define('GJ_CODE', "US");
+define('SITE_NAME', "小兵看世界");
+define('TITLENAME', "From here to the world!");
+define('EN2DEKEY', "ZgN2tj1ETs");
+define('EMAIL', "zuobq@msn.com");
 ?>
